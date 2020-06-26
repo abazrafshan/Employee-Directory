1ca45dbe1f975e765c3868b8f98a886f855ec6fa
Arash Bazrafshan <abazrafshan123@gmail.com>
2020-06-26 17:28:16 -0500

    Updates

---
+++ precache-manifest.a1b747505372d590b6697b12fc7a5772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b10fbd032c2d060ce1ec1a21794e36d0",
+    "url": "/reactapplication/index.html"
+  },
+  {
+    "revision": "975a59530df433e41f1f",
+    "url": "/reactapplication/static/css/main.0841b382.chunk.css"
+  },
+  {
+    "revision": "995d936aa1e00072921f",
+    "url": "/reactapplication/static/js/2.d3696e2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapplication/static/js/2.d3696e2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "975a59530df433e41f1f",
+    "url": "/reactapplication/static/js/main.f97250c0.chunk.js"
+  },
+  {
+    "revision": "79bd4e555d2211febc20",
+    "url": "/reactapplication/static/js/runtime-main.fb6dd984.js"
+  }
+]);
